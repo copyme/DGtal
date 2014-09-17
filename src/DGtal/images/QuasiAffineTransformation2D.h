38cf25ef8137d73d1880b37d9a6eeda8d8f4b548
Kacper Pluta <kacper.pluta@bdslabs.com.br>
2014-09-17 19:48:27 +0200

    Initial version of QuasiAffineTransformation2D

--- src/DGtal/images/QuasiAffineTransformation2D.h
+++ src/DGtal/images/QuasiAffineTransformation2D.h
@@ -42,8 +42,11 @@
 // Inclusions
 #include <iostream>
 #include "DGtal/base/Common.h"
+#include <functional>
 #include "DGtal/helpers/StdDefs.h"
 #include "DGtal/math/linalg/SimpleMatrix.h"
+#include "DGtal/images/ImageSelector.h"
+#include "DGtal/images/CImage.h"
 //////////////////////////////////////////////////////////////////////////////
 
 namespace DGtal
@@ -55,89 +58,200 @@ namespace DGtal
  * Description of class 'QuasiAffineTransformation2D' <p>
  * \brief Aim:
  */
-class QuasiAffineTransformation2D
-{
-    // ----------------------- Standard services ------------------------------
-public:
 
-    /**
-     * Destructor.
-     */
-    ~QuasiAffineTransformation2D();
+template <typename TSpace>
+class QuasiAffineTransformation2D : std::unary_function <typename TSpace::Point, typename TSpace::Point>
+{
+    ///Checking concepts
+    BOOST_CONCEPT_ASSERT(( concepts::CSpace<TSpace> ));
+    BOOST_STATIC_ASSERT(( TSpace::dimension == 2 ));
+    // ----------------------- Types ------------------------------
+public:
+    typedef Z2i::Point Point;
+    typedef Z2i::RealPoint RealPoint;
+    typedef Z2i::Vector Vector;
+    typedef SimpleMatrix < int, 2, 2 > Matrix;
 
     // ----------------------- Interface --------------------------------------
 public:
-
-    /**
-     * Writes/Displays the object on an output stream.
-     * @param out the output stream where the object is written.
-     */
-    void selfDisplay ( std::ostream & out ) const;
-
-    /**
-     * Checks the validity/consistency of the object.
-     * @return 'true' if the object is valid, 'false' otherwise.
-     */
-    bool isValid() const;
-
+      QuasiAffineTransformation2D ( Matrix matrix, int omega, Z2i::Vector vect )
+      {
+          m_matrix = matrix;
+          m_omega = omega;
+          m_vector = vect;
+      }
+      Point operator() ( const Point & aInput ) const
+      {
+          RealPoint point ( ( m_matrix * aInput + m_vector ) / m_omega);
+          return Point ( std::floor ( point[0] + 0.5f ), std::floor ( point[1] + 0.5f ) );
+      }
     // ------------------------- Protected Datas ------------------------------
-private:
-    typedef SimpleMatrix < int, 2, 2 > Matrix;
-    // ------------------------- Private Datas --------------------------------
-private:
+protected:
     Matrix m_matrix;
     int m_omega;
-    Z2i::Vector m_vector;
+    Vector m_vector;
+};
 
-    int a0, b0, c0, d0, e0, f0;
-    int a1, b1, c1, d1;
-    int b10, b11;
-    Matrix H, H0;
-    int alpha0, alpha1, beta1;
-    Z2i::Vector U0, U1;
-    std::vector<Z2i::Point> pavings;
-    std::vector<Z2i::Point> pavingsRemainder; // remainders of the canonical paving points
-
-    // ------------------------- Hidden services ------------------------------
+template <typename TSpace>
+class BackwardsQuasiAffineTransformation2D: std::unary_function < typename TSpace::Point, typename TSpace::Point >
+{
+    ///Checking concepts
+    BOOST_CONCEPT_ASSERT(( concepts::CSpace<TSpace> ));
+    BOOST_STATIC_ASSERT(( TSpace::dimension == 2 ));
+    // ----------------------- Types ------------------------------
+public:
+    typedef Z2i::Point Point;
+    typedef Z2i::RealPoint RealPoint;
+    typedef Z2i::Vector Vector;
+    typedef std::pair < typename TSpace::Point, typename TSpace::Point > Bounds;
+    typedef SimpleMatrix < int, 2, 2 > Matrix;
+    // ----------------------- Interface --------------------------------------
+public:
+    BackwardsQuasiAffineTransformation2D ( const Matrix & matrix, int omega, Z2i::Vector vect )
+    {
+        int det = matrix.determinant();
+        m_matrix(0,0) = matrix(1,1) * omega;
+        m_matrix(1,0) = -matrix(1,0) * omega;
+        m_matrix(0,1) = -matrix(0,1) * omega;
+        m_matrix(1,1) = matrix(0,0) * omega;
+        m_vector = ( Matrix() - matrix ) * vect;
+        m_omega = std::abs ( det );
+    }
+    Point operator() ( const Point & aInput ) const
+    {
+        RealPoint point = ( m_matrix * aInput + m_vector) / m_omega;
+        return Point ( std::floor ( point[0] + 0.5f ), std::floor ( point[1] + 0.5f ) );
+    }
+    // ------------------------- Protected Datas ------------------------------
 protected:
+    Matrix m_matrix;
+    int m_omega;
+    Vector m_vector;
+};
 
-    /**
-     * Constructor.
-     * Forbidden by default (protected to avoid g++ warnings).
-     */
-    QuasiAffineTransformation2D();
+template <typename TImage>
+class QuasiAffineTransformer2D
+{
+    ///Checking concepts
+    BOOST_CONCEPT_ASSERT(( CImage < TImage > ));
+    BOOST_STATIC_ASSERT(( TImage::Domain::dimension == 2 ));
+    // ----------------------- Types ------------------------------
+public:
+    typedef Z2i::Point Point;
+    typedef Z2i::RealPoint RealPoint;
+    typedef Z2i::Vector Vector;
+    typedef SimpleMatrix < int, 2, 2 > Matrix;
+    // ----------------------- Interface --------------------------------------
+public:
+    QuasiAffineTransformer2D ( Matrix matrix, int omega, Z2i::Vector vect )
+    {
+        int det = matrix.determinant();
+        m_matrix(0,0) = matrix(1,1) * omega;
+        m_matrix(1,0) = -matrix(1,0) * omega;
+        m_matrix(0,1) = -matrix(0,1) * omega;
+        m_matrix(1,1) = matrix(0,0) * omega;
+        m_vector = ( Matrix() - matrix ) * vect;
+        m_omega = std::abs ( det );
+    }
 
-private:
+    // Just calculating starting point and steps
+    void BackwardsNN ( const TImage & input, TImage & output )
+    {
+        Point start = m_matrix * output.domain().lowerBound() + m_vector;
+        Vector IncrX = m_matrix * Vector ( 1, - ( output.domain().upperBound()[1] - output.domain().lowerBound()[1] + 1 ) );
+        Vector IncrY = m_matrix * Vector ( 0, 1 );
 
-    /**
-     * Copy constructor.
-     * @param other the object to clone.
-     * Forbidden by default.
-     */
-    QuasiAffineTransformation2D ( const QuasiAffineTransformation2D & other );
+        for ( int i = output.domain().lowerBound()[0]; i <= output.domain().upperBound()[0]; i++ )
+        {
+            for ( int j = output.domain().lowerBound()[1]; j <= output.domain().upperBound()[1]; j++ )
+            {
+                Point point ( std::floor ( ( start[0] / m_omega ) + 0.5f ), std::floor ( ( start[1] / m_omega ) + 0.5f ) );
+                if ( input.domain().isInside ( point ) )
+                    output.setValue ( Point ( i, j ), input ( point ) );
+                start += IncrY;
+            }
+            start += IncrX;
+        }
+    }
 
-    /**
-     * Assignment.
-     * @param other the object to copy.
-     * @return a reference on 'this'.
-     * Forbidden by default.
-     */
-    QuasiAffineTransformation2D & operator= ( const QuasiAffineTransformation2D & other );
+    // ------------------------- Protected Datas ------------------------------
+protected:
+    Matrix m_matrix;
+    int m_omega;
+    Vector m_vector;
+};
 
-    // ------------------------- Internals ------------------------------------
-private:
-
-}; // end of class QuasiAffineTransformation2D
-
-
+/////////////////////////////////////////////////////////////////////////////
+// Template class DomainRigidTransformation2D
 /**
- * Overloads 'operator<<' for displaying objects of class 'QuasiAffineTransformation2D'.
- * @param out the output stream where the object is written.
- * @param object the object of class 'QuasiAffineTransformation2D' to write.
- * @return the output stream after the writing.
- */
-std::ostream&
-operator<< ( std::ostream & out, const QuasiAffineTransformation2D & object );
+     * Description of template functor like class 'DomainRigidTransformation2D' <p>
+     * \brief Aim: implements bounds of transformed domain.
+     *
+     * @tparam TDomain a 2 dimensional domain.
+     * @tparam TRigidTransformFunctor a functor which represent two dimensional rigid transformation.
+     *
+     * @see exampleRigidtransformation2d.cpp
+     */
+template <typename TDomain, typename TRigidTransformFunctor >
+class DomainQuasiAffineTransformation2D :
+        std::unary_function < std::pair < typename TDomain::Point, typename TDomain::Point >, TDomain>
+{
+    ///Checking concepts
+    BOOST_STATIC_ASSERT(( TDomain::dimension == 2 ));
+    BOOST_CONCEPT_ASSERT(( concepts::CDomain<TDomain> ));
+
+    // ----------------------- Types ------------------------------
+public:
+    typedef std::pair < typename TDomain::Space::Point, typename TDomain::Space::Point > Bounds;
+
+    // ----------------------- Interface --------------------------------------
+public:
+    /**
+       * Constructor.
+       * @param aRigidFunctor  - rigid transformation functor.
+       */
+    DomainQuasiAffineTransformation2D ( TRigidTransformFunctor & aRigidFunctor ) : transform ( aRigidFunctor ) {}
+
+    /**
+       * Operator
+       *
+       * @return bounds of the transformed domain.
+       */
+    inline
+    Bounds operator()( const TDomain & aInput ) const
+    {
+        typedef typename TDomain::Point Point;
+        Point points[4];
+        points[0] = transform ( aInput.lowerBound() );
+        points[1] = transform ( aInput.upperBound() );
+        points[2] = transform ( Point ( aInput.upperBound()[0], aInput.lowerBound()[1] ) );
+        points[3] = transform ( Point ( aInput.lowerBound()[0], aInput.upperBound()[1] ) );
+
+        Point t_min ( INT_MAX, INT_MAX ), t_max ( INT_MIN, INT_MIN );
+        for ( unsigned int i = 0; i < 4 ; i++ )
+        {
+            if ( points[i][0] < t_min[0] )
+                t_min[0] = points[i][0];
+            if ( points[i][1] < t_min[1] )
+                t_min[1] = points[i][1];
+
+            if ( points[i][0] > t_max[0] )
+                t_max[0] = points[i][0];
+            if ( points[i][1] > t_max[1] )
+                t_max[1] = points[i][1];
+        }
+
+        Bounds bounds;
+        bounds.first = t_min;
+        bounds.second = t_max;
+        return bounds;
+    }
+
+    // ------------------------- Protected Datas ------------------------------
+protected:
+    TRigidTransformFunctor & transform;
+};
+
 
 
 } // namespace DGtal
@@ -146,7 +260,7 @@ operator<< ( std::ostream & out, const QuasiAffineTransformation2D & object );
 ///////////////////////////////////////////////////////////////////////////////
 // Includes inline functions.
 #if !defined(BUILD_INLINE)
-#include "DGtal/images/QuasiAffineTransformation2D.ih"
+//#include "DGtal/images/QuasiAffineTransformation2D.ih"
 #endif
 
 
---
+++ tests/images/testQuasiAffineTransformation2D.cpp
@@ -0,0 +1,118 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file testtestQuasiAffineTransformation2D.cpp
+ * @ingroup Tests
+ * @author Kacper Pluta (\c kacper.pluta@esiee.fr )
+ * Laboratoire d'Informatique Gaspard-Monge - LIGM, France
+ *
+ * @date 2014/09/16
+ *
+ * Functions for testing class testQuasiAffineTransformation2D.
+ *
+ * This file is part of the DGtal library.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include "DGtal/base/Common.h"
+#include "ConfigTest.h"
+#include "DGtal/helpers/StdDefs.h"
+#include "DGtal/images/QuasiAffineTransformation2D.h"
+#include "DGtal/io/readers/PGMReader.h"
+#include "DGtal/io/writers/GenericWriter.h"
+#include "DGtal/images/ImageSelector.h"
+#include "DGtal/images/ConstImageAdapter.h"
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace DGtal;
+using namespace functors;
+///////////////////////////////////////////////////////////////////////////////
+// Functions for testing class testQuasiAffineTransformation2D.
+///////////////////////////////////////////////////////////////////////////////
+/**
+ * Example of a test. To be completed.
+ *
+ */
+bool testtestQuasiAffineTransformation2D()
+{
+  unsigned int nbok = 0;
+  unsigned int nb = 0;
+  
+  trace.beginBlock ( "Testing block ..." );
+  nbok += true ? 1 : 0; 
+  nb++;
+  trace.info() << "(" << nbok << "/" << nb << ") "
+	       << "true == true" << std::endl;
+  trace.endBlock();
+  
+  return nbok == nb;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Standard services - public :
+
+int main( int argc, char** argv )
+{
+  typedef QuasiAffineTransformation2D<Z2i::Space> QuasiAffineTrans;
+  typedef BackwardsQuasiAffineTransformation2D <Z2i::Space> BackwardsNN2D;
+  typedef ImageSelector<Z2i::Domain, unsigned char>::Type Image;
+  typedef QuasiAffineTransformer2D < Image > QuasiAffineTransformer2D;
+  typedef ConstImageAdapter<Image, Z2i::Domain, BackwardsNN2D, Image::Value, Identity > MyImageBackwardAdapter;
+  typedef DomainQuasiAffineTransformation2D<Z2i::Domain, QuasiAffineTrans> DomainQuasiAffineTrans;
+  typedef DomainQuasiAffineTransformation2D<Z2i::Domain, QuasiAffineTrans>::Bounds Bounds;
+  QuasiAffineTrans::Matrix mat;
+
+  mat(0,0) = 3;
+  mat(0,1) = 1;
+  mat(1,0) = -1;
+  mat(1,1) = 3;
+  QuasiAffineTrans trans(mat, 6, Z2i::Vector(0,0));
+  DomainQuasiAffineTrans domainTrans (  trans );
+  Image image = PGMReader<Image>::importPGM(testPath+"samples/church-small.pgm");
+  Bounds bounds = domainTrans ( image.domain() );
+
+  Identity idD;
+  BackwardsNN2D backwardsnn ( mat,6, Z2i::Vector(0,0) );
+
+  MyImageBackwardAdapter adapter ( image, Z2i::Domain ( bounds.first, bounds.second ) , backwardsnn, idD );
+
+  adapter >> "test-quasi_backNN.pgm";
+
+  QuasiAffineTransformer2D imageTransformer(mat, 6, Z2i::Vector(0,0));
+
+  Image final (Z2i::Domain ( bounds.first, bounds.second ) );
+
+  imageTransformer.BackwardsNN ( image, final );
+
+  final >> "test-image-trans-backwardsNN.pgm";
+
+  trace.beginBlock ( "Testing class testQuasiAffineTransformation2D" );
+  trace.info() << "Args:";
+  for ( int i = 0; i < argc; ++i )
+    trace.info() << " " << argv[ i ];
+  trace.info() << endl;
+
+  bool res = testtestQuasiAffineTransformation2D(); // && ... other tests
+  trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
+  trace.endBlock();
+//  return res ? 0 : 1;
+  return 0;
+}
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
